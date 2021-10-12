601841f8f2bd0ec1f52187a5e6d5067c2985925a
Localt <85951918+Localtings@users.noreply.github.com>
2021-10-12 10:04:13 +0700

    Update EffectModule.lua

--- EffectModule.lua
+++ EffectModule.lua
@@ -1,8 +1,7 @@
 --[[
-	USAGE:
-	module.NEW_TWEEN() - Starts the module.
-	module:GenerateParticles(Target, Object) - Play Effect (Particles)
-	module:GenerateMeshEffect(Target, Object, Duration, Desired Size) - Play Effect (Meshes)
+	module.NEW_TWEEN() 
+	module:GenerateParticles(Target, Object)
+	module:GenerateMeshEffect(Target, Object, Duration, Desired Size, How many studs will the meshes move foward?)
 ]]
 
 --Metatable--
@@ -17,23 +16,32 @@ effect.NEW_TWEEN = function()
 end
 
 --Child--
-function effect:GenerateParticles(t, o)
-	local hiteffect = o
-	hiteffect.Parent = t
-	hiteffect:Emit(1)
+function effect:GenerateParticles(_time, _object, _emit)
+	local hiteffect = _object
+	hiteffect.Parent = _time
+	hiteffect:Emit(_emit)
 	game.Debris:AddItem(hiteffect,1)
 end
 
-function effect:GenerateMeshEffect(t, o, ti, size)
+function effect:GenerateMeshEffect(_target, _object, _time, size, moveFoward)
 	local tweenservice = game:GetService("TweenService")
-	local HiteffectBall = function(Target, Object, T_ime, Size)
-		local ClonedBall = Object
-		ClonedBall.Parent = Target
-		ClonedBall.CFrame = Target.CFrame * CFrame.new(math.random(-1,1),math.random(-1,1),math.random(-1,1))
-		ClonedBall.CFrame = CFrame.new(ClonedBall.Position, Target.Position)
-		game.Debris:AddItem(ClonedBall,1)
-		tweenservice:Create(ClonedBall,TweenInfo.new(T_ime,Enum.EasingStyle.Quad,Enum.EasingDirection.InOut),{CFrame = ClonedBall.CFrame + ClonedBall.CFrame.lookVector * -7,Transparency = 1,Size = size:Play()
-	end	
-	HiteffectBall(t, o, ti, size)
+	local ClonedBall = _object
+	ClonedBall.Parent = _target
+	ClonedBall.CFrame = _target.CFrame * CFrame.new(math.random(-1,1),math.random(-1,1),math.random(-1,1))
+	ClonedBall.CFrame = CFrame.new(ClonedBall.Position, _target.Position)
+	game.Debris:AddItem(ClonedBall,1)
+	
+	tweenservice:Create(
+		ClonedBall,
+		TweenInfo.new(
+			_time,
+			Enum.EasingStyle.Quad,
+			Enum.EasingDirection.InOut
+		),{
+			CFrame = ClonedBall.CFrame + ClonedBall.CFrame.lookVector * -moveFoward,
+			Transparency = 1,
+			Size = size
+	 	}):Play()
 end
+
 return effect
